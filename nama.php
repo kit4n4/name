e668f9dd2c849c2cb832b4dc46bed212651010c6
kit4n4 <69968472+kit4n4@users.noreply.github.com>
2022-06-02 12:46:53 +0700

    Add files via upload

---
+++ nama.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html>
+<html><head>
+
+<title>Fake Indonesian Name Generator | Indonesia Names</title>
+<meta name="description" content="This site is indonesian name generator. You can generate fake indonesian name characters using this tool." />
+<meta name="keywords" content="name generator, indonesian name" /> 
+
+		<link rel="stylesheet" href="./repro/style.css" type="text/css">
+	
+	<script src="./repro/xenforo.js"></script>
+	<body> <!-- Start Fixed Template Info Header -->
+
+		<div class="background">
+			<div id="bg">background</div>
+			<div class="page">
+				<div class="sidebar">
+					<div id="logo">
+						<a href="http://www.ninjaname.net"><img src="./repro/logo.gif" alt=""></a>
+					</div>
+					<ul>
+						<li class="selected"><a href="/">Ninja Name</a></li>
+						<li class="selected"><a href="super_hero.php">Superhero Name</a></li>
+						<li class="selected"><a href="medieval_name.php">Medieval Name</a></li>
+						<li class="selected"><a href="city_name.php">City Name</a></li>
+						<li class="selected"><a href="restaurant_name.php">Restaurant Name</a></li>
+						<li class="selected"><a href="fake_name.php">Fake Name</a></li>
+						<li class="selected"><a href="french_name.php">French Name</a></li>
+						<li class="selected"><a href="german_name.php">German Name</a></li>
+						<li class="selected"><a href="dutch_name.php">Dutch Name</a></li>
+						<li class="selected"><a href="american_name.php">American Name</a></li>
+						
+						<li class="selected"><a href="african_name.php">African Name</a></li>
+						<li class="selected"><a href="indian_name.php">Indian Name</a></li>
+						<li class="selected"><a href="chinese_name.php">Chinese Name</a></li>
+						<li class="selected"><a href="japanese_name.php">Japanese Name</a></li>
+						<li class="selected"><a href="indonesian_name.php">Indonesian Name</a></li>
+						
+					</ul>
+					<div class="connect">
+<table width="100%"> 
+                <tr> 
+                    <td> 
+			<a href="https://twitter.com/share" class="twitter-share-button" data-count="vertical">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script> 
+                    </td> 
+		    <td style="padding-top:15px; padding-left: 15px;"> 
+			<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.ninjaname.net&amp;send=false&amp;layout=box_count&amp;width=65&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:80px;" allowTransparency="true"></iframe> 
+                    </td> 
+			
+		    <td> 
+                        
+			<!-- Place this tag where you want the +1 button to render --> 
+<g:plusone size="tall"></g:plusone> 
+ 
+<!-- Place this render call where appropriate --> 
+<script type="text/javascript"> 
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script> 
+                    </td> 
+                </tr> 
+            </table> 
+					</div>
+				</div>
+				<div class="body">
+					<div class="content">
+
+
+
+						
+<div class="title_header">
+	Indonesian Name Generator
+</div>
+
+<div style="background-color:#f9f3dd; padding:10px;">	
+
+<form method="post" action="indonesian_name.php">
+Generate:
+<select name="number_generate">
+
+<option value="1">1</option><option value="3">3</option><option value="5">5</option><option value="10">10</option><option value="20">20</option><option value="30" selected='selected'>30</option><option value="50">50</option>
+</select>
+
+<select name="gender_type">
+	
+	<option value="male" selected='selected'>Male</option>
+	<option value="female" >Female</option>
+</select>
+
+&nbsp;&nbsp;
+<input type="submit" value="Generate" name="submit"/>
+
+</h3>
+</form>
+
+</div>
+
+<br/>
+<div class="title_header" style="font-size:16px;">
+	Male Indonesian Name</div>
+
+<div style="background-color:#f9f3dd; padding:10px;">	
+&bull; Mark Kemal Santi<br/>&bull; Syahid Nurfalah<br/>&bull; Edy Abdul Baskoro<br/>&bull; Andriawan Hilmiansyah<br/>&bull; Fitra Syabantika<br/>&bull; Izhar Qodir<br/>&bull; Adhim Riahdita<br/>&bull; Priyohadi Herdani<br/>&bull; Fatahillah Oktaviaman<br/>&bull; Rico Pratiwi<br/>&bull; Azhar Journalisanda<br/>&bull; Cakra Shabrina<br/>&bull; Nicolas Azzahrani<br/>&bull; Yutama Fauziyah<br/>&bull; Andhicha Zulfi Sapto<br/>&bull; Marvel Permatasari<br/>&bull; Firdaus Randhika<br/>&bull; Emir Oktavia<br/>&bull; Roy Nustantomo<br/>&bull; Jeremiah Ariani<br/>&bull; Michael Niroha<br/>&bull; Humam Suryo<br/>&bull; Zulfi Damayanti<br/>&bull; Aswin Juliyanto<br/>&bull; Reynard Gorat<br/>&bull; Pandu Lomo<br/>&bull; Dede Hasaniy<br/>&bull; Arthur Pambudi<br/>&bull; Romario Laelasari<br/>&bull; Bryan Nisa<br/>
+</div>
+
+
+	<div style="padding-top:20px;">
+	
+
+<script type="text/javascript">
+var infolinks_pid = 1740909;
+var infolinks_wsid = 4;
+</script>
+<script type="text/javascript" src="http://resources.infolinks.com/js/infolinks_main.js"></script>
+	
+	<input type="hidden" name="IL_IN_TAG" value="2"/>
+	</div>
+	
+</div>
+
+					<div class="footer">
+						<p>. Copyright &copy; 2011-. <a href="http://www.ninjaname.net">Ninja Name Generator</a>. All rights reserved. Template by freewebsitetemplates.com</p>
+					</div>
+				</div>
+		</div>
+				
+</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48864541-1', 'ninjaname.net');
+  ga('send', 'pageview');
+
+</script>
+
+	
+  </body></html>          
